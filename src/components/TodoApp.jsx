4ca26398df12706f1904dae9c7dff55d5bfbce5e
akbrsazish <sazishaliakbar@gmail.com>
2023-07-24 20:54:22 +0330

    create header component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,8 +1,8 @@
+import Header from "@/components/Header"
 const TodoApp = () => {
     return (
         <>
-            <h1>Hello Sazish</h1>
-            <p>I am a react developer </p>
+            <Header />
         </>
     )
 }
